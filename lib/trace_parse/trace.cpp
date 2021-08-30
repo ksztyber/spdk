9311acf76828eb20e4d14a0bf1fe943a3eb2a183
Konrad Sztyber <konrad.sztyber@intel.com>
2021-08-30 16:02:50 +0200

    [WIP] lib/trace_parse: map/unmap the shared memory
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: Ia575101532c612b185bd971c69157623b52b9e81

--- lib/trace_parse/trace.cpp
+++ lib/trace_parse/trace.cpp
@@ -32,18 +32,115 @@
  */
 
 #include "spdk/stdinc.h"
+#include "spdk/log.h"
 #include "spdk/trace.h"
+
+struct spdk_trace_parser {
+	struct spdk_trace_histories	*histories;
+	size_t				map_size;
+	int				fd;
+};
+
+static struct spdk_trace_parser *
+init(const struct spdk_trace_parser_opts *opts)
+{
+	struct spdk_trace_parser *parser;
+	struct stat stat;
+	int rc;
+
+	parser = (struct spdk_trace_parser *)calloc(1, sizeof(*parser));
+	if (parser == NULL) {
+		return NULL;
+	}
+
+	switch (opts->mode) {
+	case SPDK_TRACE_PARSER_MODE_FILE:
+		parser->fd = open(opts->filename, O_RDONLY);
+		break;
+	case SPDK_TRACE_PARSER_MODE_SHM:
+		parser->fd = shm_open(opts->filename, O_RDONLY, 0600);
+		break;
+	default:
+		SPDK_ERRLOG("Invalid mode: %d\n", opts->mode);
+		parser->fd = -1;
+		goto error;
+	}
+
+	if (parser->fd < 0) {
+		SPDK_ERRLOG("Could not open trace file: %s (%d)\n", opts->filename, errno);
+		goto error;
+	}
+
+	rc = fstat(parser->fd, &stat);
+	if (rc < 0) {
+		SPDK_ERRLOG("Could not get size of trace file: %s\n", opts->filename);
+		goto error;
+	}
+
+	if ((size_t)stat.st_size < sizeof(*parser->histories)) {
+		SPDK_ERRLOG("Invalid trace file: %s\n", opts->filename);
+		goto error;
+	}
+
+	/* Map the header of trace file */
+	parser->map_size = sizeof(*parser->histories);
+	parser->histories = (struct spdk_trace_histories *)mmap(NULL, parser->map_size, PROT_READ,
+			    MAP_SHARED, parser->fd, 0);
+	if (parser->histories == MAP_FAILED) {
+		SPDK_ERRLOG("Could not mmap trace file: %s\n", opts->filename);
+		goto error;
+	}
+
+	/* Remap the entire trace file */
+	parser->map_size = spdk_get_trace_histories_size(parser->histories);
+	munmap(parser->histories, sizeof(*parser->histories));
+	if ((size_t)stat.st_size < parser->map_size) {
+		SPDK_ERRLOG("Trace file %s is not a valid\n", opts->filename);
+		goto error;
+	}
+	parser->histories = (struct spdk_trace_histories *)mmap(NULL, parser->map_size, PROT_READ,
+			    MAP_SHARED, parser->fd, 0);
+	if (parser->histories == MAP_FAILED) {
+		SPDK_ERRLOG("Could not mmap trace file: %s\n", opts->filename);
+		goto error;
+	}
+
+	return parser;
+error:
+	spdk_trace_parser_cleanup(parser);
+	return NULL;
+}
+
+static void
+cleanup(struct spdk_trace_parser *parser)
+{
+	if (parser == NULL) {
+		return;
+	}
+
+	if (parser->histories != NULL) {
+		munmap(parser->histories, parser->map_size);
+	}
+
+	if (parser->fd > 0) {
+		close(parser->fd);
+	}
+
+	free(parser);
+}
 
 extern "C" {
 
 	struct spdk_trace_parser *
 	spdk_trace_parser_init(const struct spdk_trace_parser_opts *opts)
 	{
-		return NULL;
+		return init(opts);
 	}
 
 	void
 	spdk_trace_parser_cleanup(struct spdk_trace_parser *parser)
-	{}
+	{
+		cleanup(parser);
+	}
 
 } /* extern "C" */
