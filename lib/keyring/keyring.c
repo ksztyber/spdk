9b185db3ba92cac65f23899aff78a79669f46549
Konrad Sztyber <konrad.sztyber@intel.com>
2023-11-28 16:16:27 +0100

    lib: add keyring library
    The purpose of this library is to provide other libraries with an
    interface for retrieving cryptographic keys.  This interface consists of
    functions managing the keyring (add/remove), keeping track of key
    references (get/put), and a set of callbacks used to define modules
    responsible for providing access to the keys in a secure manner.
    
    Key objects (called spdk_key) are identified by a unique name.  They
    don't hold the keying material themselves, but only act as references to
    the actual key.  Once the key is needed to perform a crypto operation,
    this reference is used to retrieve keying material.  These objects are
    refcounted, so it is safe to remove a key from the keyring while it's
    still in use.  If that happens, the spdk_key object remains valid, but
    it cannot be used to obtain the key (i.e. spdk_key_get_key() will result
    in -ENOKEY).
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: I5324fa28d5828a989b003bb5f715cfcac2077262

---
+++ lib/keyring/keyring.c
@@ -0,0 +1,285 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2023 Intel Corporation. All rights reserved.
+ */
+
+#include "spdk/keyring.h"
+#include "spdk/keyring_module.h"
+#include "spdk/log.h"
+#include "spdk/queue.h"
+#include "spdk/string.h"
+
+struct spdk_key {
+	char				*name;
+	int				refcnt;
+	bool				removed;
+	struct spdk_keyring_module	*module;
+	TAILQ_ENTRY(spdk_key)		tailq;
+};
+
+struct spdk_keyring {
+	pthread_mutex_t				mutex;
+	TAILQ_HEAD(, spdk_keyring_module)	modules;
+	TAILQ_HEAD(, spdk_key)			keys;
+	TAILQ_HEAD(, spdk_key)			removed_keys;
+};
+
+static struct spdk_keyring g_keyring = {
+	.mutex = PTHREAD_MUTEX_INITIALIZER,
+	.keys = TAILQ_HEAD_INITIALIZER(g_keyring.keys),
+	.removed_keys = TAILQ_HEAD_INITIALIZER(g_keyring.removed_keys),
+	.modules = TAILQ_HEAD_INITIALIZER(g_keyring.modules),
+};
+
+static struct spdk_keyring_module *
+keyring_find_module(const char *name)
+{
+	struct spdk_keyring_module *module;
+
+	TAILQ_FOREACH(module, &g_keyring.modules, tailq) {
+		if (strcmp(module->name, name) == 0) {
+			return module;
+		}
+	}
+
+	return NULL;
+}
+
+static struct spdk_key *
+keyring_find_key(const char *name)
+{
+	struct spdk_key *key;
+
+	/* g_keyring.mutex must be held */
+	TAILQ_FOREACH(key, &g_keyring.keys, tailq) {
+		if (strcmp(key->name, name) == 0) {
+			return key;
+		}
+	}
+
+	return NULL;
+}
+
+static void
+keyring_free_key(struct spdk_key *key)
+{
+	assert(key->refcnt == 0);
+
+	free(key->name);
+	free(key);
+}
+
+static void
+keyring_put_key(struct spdk_key *key)
+{
+	assert(key->refcnt > 0);
+	key->refcnt--;
+
+	if (key->refcnt == 0) {
+		assert(key->removed);
+		TAILQ_REMOVE(&g_keyring.removed_keys, key, tailq);
+		keyring_free_key(key);
+	}
+}
+
+int
+spdk_keyring_add(const struct spdk_key_opts *opts)
+{
+	struct spdk_key *key = NULL;
+	struct spdk_keyring_module *module;
+	int rc = 0;
+
+	pthread_mutex_lock(&g_keyring.mutex);
+	if (keyring_find_key(opts->name) != NULL) {
+		SPDK_ERRLOG("Key '%s' already exists\n", opts->name);
+		rc = -EEXIST;
+		goto out;
+	}
+
+	module = keyring_find_module(opts->module);
+	if (module == NULL) {
+		SPDK_ERRLOG("Could not find module '%s'\n", opts->module);
+		rc = -ENOENT;
+		goto out;
+	}
+
+	key = calloc(1, sizeof(*key) + module->get_ctx_size());
+	if (key == NULL) {
+		rc = -ENOMEM;
+		goto out;
+	}
+
+	key->name = strdup(opts->name);
+	if (key->name == NULL) {
+		rc = -ENOMEM;
+		goto out;
+	}
+
+	rc = module->add_key(key, opts->opts);
+	if (rc != 0) {
+		SPDK_ERRLOG("Failed to add key '%s' to the keyring\n", opts->name);
+		goto out;
+	}
+
+	key->module = module;
+	key->refcnt = 1;
+	TAILQ_INSERT_TAIL(&g_keyring.keys, key, tailq);
+out:
+	pthread_mutex_unlock(&g_keyring.mutex);
+	if (rc != 0 && key != NULL) {
+		keyring_free_key(key);
+	}
+
+	return rc;
+}
+
+static void
+keyring_remove_key(struct spdk_key *key)
+{
+	assert(!key->removed);
+	key->removed = true;
+	key->module->remove_key(key);
+	TAILQ_REMOVE(&g_keyring.keys, key, tailq);
+	TAILQ_INSERT_TAIL(&g_keyring.removed_keys, key, tailq);
+	keyring_put_key(key);
+}
+
+void
+spdk_keyring_remove(const char *name)
+{
+	struct spdk_key *key;
+
+	pthread_mutex_lock(&g_keyring.mutex);
+	key = keyring_find_key(name);
+	if (key == NULL) {
+		SPDK_WARNLOG("Key '%s' does not exist\n", name);
+		goto out;
+	}
+
+	keyring_remove_key(key);
+out:
+	pthread_mutex_unlock(&g_keyring.mutex);
+}
+
+struct spdk_key *
+spdk_keyring_get(const char *name)
+{
+	struct spdk_key *key;
+
+	pthread_mutex_lock(&g_keyring.mutex);
+	key = keyring_find_key(name);
+	if (key == NULL) {
+		goto out;
+	}
+
+	key->refcnt++;
+out:
+	pthread_mutex_unlock(&g_keyring.mutex);
+
+	return key;
+}
+
+void
+spdk_keyring_put(struct spdk_key *key)
+{
+	if (key == NULL) {
+		return;
+	}
+
+	pthread_mutex_lock(&g_keyring.mutex);
+	keyring_put_key(key);
+	pthread_mutex_unlock(&g_keyring.mutex);
+}
+
+const char *
+spdk_key_get_name(struct spdk_key *key)
+{
+	return key->name;
+}
+
+int
+spdk_key_get_key(struct spdk_key *key, void *buf, int len)
+{
+	struct spdk_keyring_module *module = key->module;
+
+	if (key->removed) {
+		return -ENOKEY;
+	}
+
+	return module->get_key(key, buf, len);
+}
+
+void *
+spdk_key_get_ctx(struct spdk_key *key)
+{
+	return key + 1;
+}
+
+void
+spdk_keyring_register_module(struct spdk_keyring_module *module)
+{
+	assert(keyring_find_module(module->name) == NULL);
+	TAILQ_INSERT_TAIL(&g_keyring.modules, module, tailq);
+}
+
+int
+spdk_keyring_init(void)
+{
+	struct spdk_keyring_module *module, *tmp;
+	int rc = 0;
+
+	TAILQ_FOREACH(module, &g_keyring.modules, tailq) {
+		if (module->init != NULL) {
+			rc = module->init();
+			if (rc != 0) {
+				SPDK_ERRLOG("Failed to initialize module %s: %s\n", module->name,
+					    spdk_strerror(-rc));
+				break;
+			}
+		}
+
+		SPDK_INFOLOG(keyring, "Initialized module %s\n", module->name);
+	}
+
+	if (rc != 0) {
+		TAILQ_FOREACH(tmp, &g_keyring.modules, tailq) {
+			if (tmp == module) {
+				break;
+			}
+			if (tmp->cleanup != NULL) {
+				tmp->cleanup();
+			}
+		}
+	}
+
+	return rc;
+}
+
+void
+spdk_keyring_cleanup(void)
+{
+	struct spdk_keyring_module *module;
+	struct spdk_key *key;
+
+	while (!TAILQ_EMPTY(&g_keyring.keys)) {
+		key = TAILQ_FIRST(&g_keyring.keys);
+		if (!key->removed) {
+			keyring_remove_key(key);
+		}
+	}
+
+	while (!TAILQ_EMPTY(&g_keyring.removed_keys)) {
+		key = TAILQ_FIRST(&g_keyring.removed_keys);
+		SPDK_WARNLOG("Key '%s' still has %d references\n", key->name, key->refcnt);
+		key->refcnt = 0;
+		TAILQ_REMOVE(&g_keyring.removed_keys, key, tailq);
+		keyring_free_key(key);
+	}
+
+	TAILQ_FOREACH(module, &g_keyring.modules, tailq) {
+		if (module->cleanup != NULL) {
+			module->cleanup();
+		}
+	}
+}
+
+SPDK_LOG_REGISTER_COMPONENT(keyring)
