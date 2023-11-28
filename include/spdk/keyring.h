be855c628843dcbf31be99aa49cf3664f7f94817
Konrad Sztyber <konrad.sztyber@intel.com>
2023-11-28 16:16:27 +0100

    lib: add keyring library
    The purpose of this library is to provide other libraries with an
    interface for retrieving cryptographic keys.  This interface consists of
    functions managing the keyring (add/remove), keeping track of key
    references (get/put), and a set of callbacks used to define modules
    responsible for providing access to the keys in a secure manner.
    
    Key objects (called spdk_key) are identified by a unique name.  They
    don't hold the keying material themselves, but only act as references to
    the actual key.  Once user needs the key for crypto operations, this
    reference can be used to retrieve keying material.  These objects are
    refcounted, so it's safe to remove a key from the keyring, even if it's
    still in use.  If that happens, the spdk_key objects are still valid,
    but it's not possible to use them to get a key (i.e. spdk_key_get_key()
    will result in -ENOKEY).
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: I5324fa28d5828a989b003bb5f715cfcac2077262

---
+++ include/spdk/keyring.h
@@ -0,0 +1,91 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2023 Intel Corporation. All rights reserved.
+ */
+
+#ifndef SPDK_KEYRING_H
+#define SPDK_KEYRING_H
+
+#include "spdk/stdinc.h"
+#include "spdk/json.h"
+
+struct spdk_key;
+
+struct spdk_key_opts {
+	/** Size of this structure */
+	size_t size;
+	/** Name of the key */
+	const char *name;
+	/** Name of the keyring module */
+	const char *module;
+	/** Module-specific options */
+	const struct spdk_json_val *opts;
+};
+
+/**
+ * Add a key to the keyring.
+ *
+ * \param opts Key options.
+ *
+ * \return 0 on success, negative errno otherwise.
+ */
+int spdk_keyring_add(const struct spdk_key_opts *opts);
+
+/**
+ * Remove a key from the keyring.
+ *
+ * \param name Name of the key to remove.
+ */
+void spdk_keyring_remove(const char *name);
+
+/**
+ * Get a reference to a key from the keyring.  The key must have been added to the keyring by
+ * the appropriate keyring module.  The reference will be kept alive until its released via
+ * `spdk_keyring_put()`.  If the key is removed from the keyring, the reference is kept alive, but
+ * the key won't be usable anymore.
+ *
+ * \param name Name of a key.
+ *
+ * \return Reference to a key or NULL if the key doesn't exist.
+ */
+struct spdk_key *spdk_keyring_get(const char *name);
+
+/**
+ * Release a reference to a key obtained from `spdk_keyring_get()`.
+ *
+ * \param key Reference to a key.
+ */
+void spdk_keyring_put(struct spdk_key *key);
+
+/**
+ * Get the name of a key.
+ *
+ * \param key Reference to a key.
+ *
+ * \return Name of the key.
+ */
+const char *spdk_key_get_name(struct spdk_key *key);
+
+/**
+ * Retrieve keying material form a key reference.
+ *
+ * \param key Reference to a key.
+ * \param buf Buffer to write the data to.
+ * \param len Size of the `buf` buffer.
+ *
+ * \return The number of bytes written to `buf` or negative errno on error.
+ */
+int spdk_key_get_key(struct spdk_key *key, void *buf, int len);
+
+/**
+ * Initialize the keyring library.
+ *
+ * \return 0 on success, negative errno otherwise.
+ */
+int spdk_keyring_init(void);
+
+/**
+ * Free any resources acquired by the keyring library.  This function will free all of the keys.
+ */
+void spdk_keyring_cleanup(void);
+
+#endif /* SPDK_KEYRING_H */
---
+++ include/spdk/keyring_module.h
@@ -0,0 +1,54 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2023 Intel Corporation. All rights reserved.
+ */
+
+#ifndef SPDK_KEYRING_MODULE_H
+#define SPDK_KEYRING_MODULE_H
+
+#include "spdk/stdinc.h"
+#include "spdk/keyring.h"
+#include "spdk/queue.h"
+
+struct spdk_keyring_module {
+	/** Name of the module */
+	const char *name;
+
+	/** Initializes a module, called during keyring's initialization */
+	int (*init)(void);
+	/** Clean up resources allocated by a module.  Called during keyring's cleanup  */
+	void (*cleanup)(void);
+	/** Add a key to the keyring */
+	int (*add_key)(struct spdk_key *key, const struct spdk_json_val *opts);
+	/** Remove a key from the keyring */
+	void (*remove_key)(struct spdk_key *key);
+	/** Get keying material from a key */
+	int (*get_key)(struct spdk_key *key, void *buf, int len);
+	/** Get the size of the context associated with a key */
+	size_t (*get_ctx_size)(void);
+
+	TAILQ_ENTRY(spdk_keyring_module) tailq;
+};
+
+/**
+ * Register a keyring module.
+ *
+ * \param module Keyring module to register.
+ */
+void spdk_keyring_register_module(struct spdk_keyring_module *module);
+
+#define SPDK_KEYRING_REGISTER_MODULE(name, module) \
+static void __attribute__((constructor)) _spdk_keyring_register_##name(void) \
+{ \
+	spdk_keyring_register_module(module); \
+}
+
+/**
+ * Get pointer to the module context associated with a key.
+ *
+ * \param key Key.
+ *
+ * \return Key context.
+ */
+void *spdk_key_get_ctx(struct spdk_key *key);
+
+#endif /* SPDK_KEYRING_H */
