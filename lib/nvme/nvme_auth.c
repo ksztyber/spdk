206628b040353e7fb9ef695cb372c39d223de984
Konrad Sztyber <konrad.sztyber@intel.com>
2024-01-22 15:48:32 +0100

    nvme: initial in-band authentication structures
    This patch adds structures that will be used to implement NVMe in-band
    authentication using the DH-HMAC-CHAP protocol.  They're mostly empty for
    now, but the following patches will implement each step of this process.
    
    The authentication is executed by exchanging several messages using the
    AUTHENTICATION_SEND and AUTHENTICATION_RECEIVE commands from the Fabrics
    Command Set.  The messages sent by the host map to the SEND commands,
    while those sent by the controller map to the RECEIVE commands.
    
    The flow roughly consists of 5 steps:
     1. The host sends a AUTH_negotiate message to listing the hash function
        and Diffie-Hellman group it supports.
     2. The controller responds with a DH-HMAC-CHAP_challenge message
        selecting one of the hash functions and DH groups from those sent by
        the host along with a challenge value and an optional DH public key.
     3. The host calculates an HMAC of the challenge (optionally augmented
        with a DH secret) and sends it back to the controller using a
        DH-HMAC-CHAP_reply message along with its DH public key.  If
        bidirectional authentication is requested, it includes its own
        challenge value in that message.
     4. The controller verifies the response to the challenge received from
        the host comparing it to its HMAC calculation.  If successful, it
        sends a DH-HMAC-CHAP_success1 message with an optional response to
        the host challenge.
     5. If bidirectional authentication wasn't requested, the authentication
        process is finished at this point.  Otherwise the host verifies
        controller's response to the host's challenge and, if successful,
        sends a DH-HMAC-CHAP_success2 message.
    
    Any errors during this process are reported by sending either an
    AUTH_failure1 message (sent by the controller) or an AUTH_failure2 (sent
    by the host).
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: I84cc1574e15884e1e3d3d03e517cb38f05e12ef6

---
+++ lib/nvme/nvme_auth.c
@@ -0,0 +1,130 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2024 Intel Corporation.  All rights reserved.
+ */
+
+#include "spdk/log.h"
+#include "nvme_internal.h"
+
+#define NVME_AUTH_DATA_SIZE 4096
+
+#define AUTH_DEBUGLOG(q, fmt, ...)					\
+	SPDK_DEBUGLOG(nvme_auth, "[nqn=%s:qid=%u] " fmt,		\
+		      (q)->ctrlr->trid.subnqn, (q)->id, ## __VA_ARGS__)
+#define AUTH_ERRLOG(q, fmt, ...) \
+	SPDK_ERRLOG("[nqn=%s:qid=%u] " fmt, (q)->ctrlr->trid.subnqn, (q)->id, ## __VA_ARGS__)
+
+static const char *
+nvme_auth_get_state_name(int state)
+{
+	const char *sstr[] = {
+		[NVME_QPAIR_AUTH_STATE_NEGOTIATE] = "negotiate",
+		[NVME_QPAIR_AUTH_STATE_AWAIT_NEGOTIATE] = "await-negotiate",
+		[NVME_QPAIR_AUTH_STATE_AWAIT_CHALLENGE] = "await-challenge",
+		[NVME_QPAIR_AUTH_STATE_AWAIT_REPLY] = "await-reply",
+		[NVME_QPAIR_AUTH_STATE_AWAIT_SUCCESS1] = "await-success1",
+		[NVME_QPAIR_AUTH_STATE_AWAIT_FAILURE2] = "await-failure2",
+		[NVME_QPAIR_AUTH_STATE_DONE] = "done",
+	};
+
+	if (state < 0 || state >= (int)SPDK_COUNTOF(sstr)) {
+		return NULL;
+	}
+
+	return sstr[state];
+}
+
+static void
+nvme_auth_set_state(struct spdk_nvme_qpair *qpair, enum nvme_qpair_auth_state state)
+{
+	AUTH_DEBUGLOG(qpair, "auth state: %s\n", nvme_auth_get_state_name(state));
+	qpair->auth.state = state;
+}
+
+static void
+nvme_auth_set_failure(struct spdk_nvme_qpair *qpair, int status, bool failure2)
+{
+	if (qpair->auth.status == 0) {
+		qpair->auth.status = status;
+	}
+
+	nvme_auth_set_state(qpair, failure2 ?
+			    NVME_QPAIR_AUTH_STATE_AWAIT_FAILURE2 :
+			    NVME_QPAIR_AUTH_STATE_DONE);
+}
+
+int
+nvme_fabric_qpair_authenticate_poll(struct spdk_nvme_qpair *qpair)
+{
+	struct nvme_auth *auth = &qpair->auth;
+	struct nvme_completion_poll_status *status = qpair->poll_status;
+	enum nvme_qpair_auth_state prev_state;
+	int rc;
+
+	do {
+		prev_state = auth->state;
+
+		switch (auth->state) {
+		case NVME_QPAIR_AUTH_STATE_NEGOTIATE:
+		case NVME_QPAIR_AUTH_STATE_AWAIT_NEGOTIATE:
+		case NVME_QPAIR_AUTH_STATE_AWAIT_CHALLENGE:
+		case NVME_QPAIR_AUTH_STATE_AWAIT_REPLY:
+		case NVME_QPAIR_AUTH_STATE_AWAIT_SUCCESS1:
+		case NVME_QPAIR_AUTH_STATE_AWAIT_FAILURE2:
+			nvme_auth_set_failure(qpair, -ENOTSUP, false);
+			break;
+		case NVME_QPAIR_AUTH_STATE_DONE:
+			rc = auth->status;
+			if (qpair->poll_status != NULL && !status->timed_out) {
+				qpair->poll_status = NULL;
+				spdk_free(status->dma_data);
+				free(status);
+			}
+			break;
+		default:
+			assert(0 && "invalid state");
+			rc = -EINVAL;
+			break;
+		}
+	} while (auth->state != prev_state);
+
+	return rc;
+}
+
+int
+nvme_fabric_qpair_authenticate_async(struct spdk_nvme_qpair *qpair)
+{
+	struct spdk_nvme_ctrlr *ctrlr = qpair->ctrlr;
+	struct nvme_completion_poll_status *status;
+	struct nvme_auth *auth = &qpair->auth;
+
+	if (ctrlr->opts.chap_key == NULL) {
+		AUTH_ERRLOG(qpair, "missing DH-HMAC-CHAP key\n");
+		return -ENOKEY;
+	}
+
+	status = calloc(1, sizeof(*qpair->poll_status));
+	if (!status) {
+		AUTH_ERRLOG(qpair, "failed to allocate poll status\n");
+		return -ENOMEM;
+	}
+
+	status->dma_data = spdk_zmalloc(NVME_AUTH_DATA_SIZE, 0, NULL, SPDK_ENV_LCORE_ID_ANY,
+					SPDK_MALLOC_DMA);
+	if (!status->dma_data) {
+		AUTH_ERRLOG(qpair, "failed to allocate poll status\n");
+		free(status);
+		return -ENOMEM;
+	}
+
+	assert(qpair->poll_status == NULL);
+	qpair->poll_status = status;
+
+	nvme_robust_mutex_lock(&ctrlr->ctrlr_lock);
+	auth->tid = ctrlr->auth_tid++;
+	nvme_robust_mutex_unlock(&ctrlr->ctrlr_lock);
+
+	nvme_auth_set_state(qpair, NVME_QPAIR_AUTH_STATE_NEGOTIATE);
+
+	return 0;
+}
+SPDK_LOG_REGISTER_COMPONENT(nvme_auth)
